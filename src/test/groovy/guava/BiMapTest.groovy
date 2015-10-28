0d0d5708364f60297eb34de6f8e0ca026caa95c2
Fabian Dietenberger <piano.fabian@t-online.de>
2015-10-28 22:39:15 +0100

    Add guava bimap test

---
+++ src/test/groovy/guava/BiMapTest.groovy
@@ -0,0 +1,80 @@
+package guava
+
+import com.google.common.collect.BiMap
+import com.google.common.collect.HashBiMap
+import spock.lang.Specification
+
+
+/**
+ * This class is used as test fot the guava bimap.
+ *
+ * @author Fabian Dietenberger
+ */
+class BiMapTest extends Specification {
+
+    def "add values to a bimap"() {
+        given:
+        BiMap<String, String> englishToGerman = HashBiMap.create()
+        englishToGerman.put("one", "eins")
+        englishToGerman.put("two", "zwei")
+        englishToGerman.put("three", "drei")
+
+        expect:
+        englishToGerman.containsKey("one")
+        englishToGerman.containsValue("eins")
+        englishToGerman.size() == 3
+    }
+
+    def "inverse the bimap"() {
+        given: "a bimap"
+        BiMap<String, String> englishToGerman = HashBiMap.create()
+        englishToGerman.put("one", "eins")
+        englishToGerman.put("two", "zwei")
+        englishToGerman.put("three", "three")
+
+        when: "we inverse the bimap"
+        englishToGerman = englishToGerman.inverse()
+
+        then: "the key and values are interchanged"
+        englishToGerman.containsKey("eins")
+        englishToGerman.containsValue("one")
+
+        when: "we inverse the bimap again"
+        englishToGerman = englishToGerman.inverse()
+
+        then: "we have the original bimap"
+        englishToGerman.containsKey("one")
+        englishToGerman.containsValue("eins")
+    }
+
+    def "override exisiting values"() {
+        given: "a bimap"
+        BiMap<String, String> englishToGerman = HashBiMap.create()
+        englishToGerman.put("one", "uno")
+        englishToGerman.put("uno", "zwei")
+        englishToGerman.put("three", "three")
+
+        when: "we put an exisiting key"
+        englishToGerman.put("one", "eins")
+
+        then: "the old one gets overriden"
+        englishToGerman.containsKey("one")
+        englishToGerman.containsValue("eins")
+
+        when: "we put an existing value"
+        englishToGerman.put("uno", "eins")
+
+        then: "the bimap throws an error"
+        thrown IllegalArgumentException
+
+        when: "we force a put"
+        englishToGerman.forcePut("one", "eins")
+        englishToGerman.forcePut("two", "zwei")
+
+        then: "we overridden the existing value"
+        englishToGerman.containsKey("one")
+        englishToGerman.containsValue("eins")
+        englishToGerman.containsKey("two")
+        englishToGerman.containsValue("zwei")
+    }
+}
