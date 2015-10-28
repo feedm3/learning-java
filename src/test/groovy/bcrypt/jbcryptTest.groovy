8886752f4ddc1f46b251b7e0f1fdcf25f368c5bb
Fabian Dietenberger <piano.fabian@t-online.de>
2015-10-28 22:02:39 +0100

    Add bcrypt test

--- src/test/groovy/bcrypt/jbcryptTest.groovy
+++ src/test/groovy/bcrypt/jbcryptTest.groovy
@@ -5,45 +5,34 @@ import spock.lang.Specification
 
 
 /**
+ * This class is used as test for the jBcrypt library.
+ *
  * @author Fabian Dietenberger
  */
 class JBcryptTest extends Specification {
 
     def "create a new hash"() {
-        setup:
+        given: "a password"
         def password = 'testpassword'
 
-        when:
+        when: "we hash the password"
         def hash = BCrypt.hashpw(password, BCrypt.gensalt(9))
 
-        then:
+        then: "we get a hash"
         !hash.isEmpty()
         !hash.equals(password)
         (hash.length() == 59 || hash.length() == 60)
     }
 
     def "password and hash must be validated true"() {
-        setup:
+        given: "a password"
         def password = 'testpassword'
+
+        when: "we hash the password"
         def hash = BCrypt.hashpw(password, BCrypt.gensalt(9))
 
-        when:
-        def isPasswordCorrect = BCrypt.checkpw(password, hash)
-
-        then:
-        isPasswordCorrect
-    }
-
-    def "different password and hash must be validated false"() {
-        setup:
-        def password = 'testpassword'
-        def hash = BCrypt.hashpw(password, BCrypt.gensalt(9))
-        def differentPassword = 'wrong'
-
-        when:
-        def isPasswordCorrect = BCrypt.checkpw(differentPassword, hash)
-
-        then:
-        !isPasswordCorrect
+        then: "we can validate the password against this hash"
+        BCrypt.checkpw(password, hash)
+        !BCrypt.checkpw("wrong password", hash)
     }
 }
