194adb5c63e8ade1e59ce6b0de1e92b9d742a579
Fabian Dietenberger <piano.fabian@t-online.de>
2015-10-29 21:52:58 +0100

    Add guava immutable list test

---
+++ src/test/groovy/guava/ImmutableListTest.groovy
@@ -0,0 +1,32 @@
+package guava
+
+import com.google.common.collect.ImmutableSet
+import spock.lang.Specification
+
+
+/**
+ * This class is used as test for the guava immutable list
+ *
+ * https://github.com/google/guava/wiki/ImmutableCollectionsExplained
+ *
+ * @author Fabian Dietenberger
+ */
+class ImmutableListTest extends Specification {
+
+    def "add elements to an immutable set"() {
+        given: "an immutable set"
+        def colors = ImmutableSet.of(
+                "black",
+                "red",
+                "gold"
+        )
+
+        when: "adding or removing elements"
+        colors.add("white")
+        colors.remove("black")
+
+        then: "an exception should be thrown and the size should remain the same"
+        thrown UnsupportedOperationException
+        colors.size() == 3
+    }
+}
