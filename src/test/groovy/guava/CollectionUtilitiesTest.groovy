f779b7ba5243a5176767d8ce83ee9ab8b4dfa051
Fabian Dietenberger <piano.fabian@t-online.de>
2015-10-29 21:49:48 +0100

    Add guava collection utilities test

---
+++ src/test/groovy/guava/CollectionUtilitiesTest.groovy
@@ -0,0 +1,53 @@
+package guava
+
+import com.google.common.collect.Lists
+import com.google.common.collect.Sets
+import spock.lang.Specification
+
+
+/**
+ * Helper methods if you work with lists, sets, iterables...
+ *
+ * https://github.com/google/guava/wiki/CollectionUtilitiesExplained
+ *
+ * @author Fabian Dietenberger
+ */
+class CollectionUtilitiesTest extends Specification {
+
+    def "Lists"() {
+        given:
+        List<String> stringsEmptyList = Lists.newArrayList()
+        List<String> stringsList = Lists.newArrayList("BMW", "Audi", "VW")
+        List<String> stringsCopyList = Lists.newArrayList(stringsList)
+
+        expect:
+        stringsEmptyList.size() == 0
+        stringsList.size() == 3
+        stringsCopyList.size() == 3
+
+        when:
+        stringsList.remove("BMW")
+
+        then:
+        stringsList.size() == 2
+        stringsCopyList.size() == 3
+    }
+
+    def "Sets"() {
+        given:
+        Set<String> emptyStringsSet = Sets.newHashSet()
+        Set<String> identityHashSet = Sets.newIdentityHashSet() // uses == as comparator
+
+        expect:
+        emptyStringsSet.size() == 0
+        identityHashSet.size() == 0
+
+        when:
+        emptyStringsSet.addAll(new String("BMW"), new String("BMW"))
+        identityHashSet.addAll(new String("BMW"), new String("BMW"))
+
+        then:
+        emptyStringsSet.size() == 1
+        identityHashSet.size() == 2
+    }
+}
