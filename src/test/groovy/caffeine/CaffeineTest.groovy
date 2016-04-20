86aa2479fd9c6f167671e4ebdaa8b6243dc1f6a4
Fabian Dietenberger <fabian.dietenberger@ametras.com>
2016-04-20 14:48:05 +0200

    Add a first, simple caffeine test

--- src/test/groovy/caffeine/CaffeineTest.groovy
+++ src/test/groovy/caffeine/CaffeineTest.groovy
@@ -1,8 +1,10 @@
 package caffeine
 
+import com.github.benmanes.caffeine.cache.Cache
+import com.github.benmanes.caffeine.cache.Caffeine
 import spock.lang.Specification
 
-
+import java.util.concurrent.TimeUnit
 /**
  * This class is used as test for the caffeine library.
  *
@@ -10,4 +12,41 @@ import spock.lang.Specification
  */
 class CaffeineTest extends Specification {
 
+    /**
+     * Caffeine allows explicit control of retrieving, updating and invalidation entries.
+     */
+    def "populate a cache manual"() {
+        given:
+        Cache<Integer, String> cache = Caffeine.newBuilder()
+                .expireAfterWrite(10, TimeUnit.SECONDS)
+                .maximumSize(5)
+                .build();
+
+        when: // adding an entry direct
+        cache.put(1, "One")
+
+        then:
+        cache.getIfPresent(1) == "One"
+
+        when: // adding an entry via a loader
+        cache.get(2, { Integer key -> getStringFromExpensiveComputation(key) })
+
+        then:
+        cache.getIfPresent(2) == "Two"
+
+        when: // invalidation an entry
+        cache.invalidate(1)
+
+        then:
+        cache.getIfPresent(1) == null
+    }
+
+    def getStringFromExpensiveComputation = { int number ->
+        switch (number) {
+            case 1:
+                return "One"
+            case 2:
+                return "Two"
+        }
+    }
 }
