bc5e5277907ad8f01824f1d2d999e413847f86fe
Fabian Dietenberger <fabian.dietenberger@sinnerschrader.com>
2017-07-30 10:58:41 +0200

    Add test for default base functional interfaces

--- src/test/java/jdk8/ocp/FunctionTest.java
+++ src/test/java/jdk8/ocp/FunctionTest.java
@@ -4,7 +4,11 @@ import org.junit.Test;
 
 import com.google.common.truth.Truth;
 
+import java.util.function.Consumer;
 import java.util.function.DoubleFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import static com.google.common.truth.Truth.assertThat;
 
@@ -21,5 +25,35 @@ public class FunctionTest {
         final String valueOf = doubleFunction.apply(value);
 
         assertThat(valueOf).isEqualTo("2.0");
+    }
+
+    /**
+     * There are 4 different default base functional interfaces.
+     *
+     * - Function
+     * - Consumer
+     * - Supplier
+     * - Predicate
+     */
+    @Test
+    public void testDifferentFunctionalInterfaces() {
+        // Function: Has an input parameter and a return value
+        final Function<String, Integer> function = (value) -> { return 1; };
+        final Integer functionRestult = function.apply("test");
+        assertThat(functionRestult).isEqualTo(1);
+
+        // Consumer: Only has an input parameter
+        final Consumer<String> consumer = (value) -> {};
+        consumer.accept("test");
+
+        // Supplier: Only has an return value
+        final Supplier<Integer> supplier = () -> { return 1; };
+        final Integer supplierResult = supplier.get();
+        assertThat(supplierResult).isEqualTo(1);
+
+        // Predicate: Has an input paramter and a boolean return value
+        final Predicate<String> predicate = (value) -> { return true; };
+        final boolean predicateResult = predicate.test("test");
+        assertThat(predicateResult).isTrue();
     }
 }
